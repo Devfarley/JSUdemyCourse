683f1955464b0b48f5c923b8963a18324bcf03cf
Devfarley <66633806+Devfarley@users.noreply.github.com>
2020-12-01 12:19:01 -0700

    Lesson 2

--- 01-Fundamentals-Part-1/practiceassignments/script.js
+++
@@ -1,7 +0,0 @@
-let country = 'USA';
-let continent = 'America';
-let population = '328.2 million'
-
-console.log(country)
-console.log(continent)
-console.log(population)
---
+++ 01-Fundamentals-Part-1/practiceassignments/valuesAndVariables.js
@@ -0,0 +1,32 @@
+/*
+let country = 'USA';
+let continent = 'America';
+let population = '328.2 million'
+
+console.log(country)
+console.log(continent)
+console.log(population)
+*/
+
+// let javascriptIsFun = true;
+// console.log(javascriptIsFun);
+
+// // console.log(typeof true);
+// console.log(typeof javascriptIsFun);
+// // console.log(typeof 23);
+// // console.log(typeof 'true');
+
+// javascriptIsFun = 'YES!';
+// console.log(typeof javascriptIsFun);
+
+let age = 30;
+age = 30;
+
+const birthYear = 1991;
+// birthYear = 1990;
+
+const job;
+
+var job = 'programmer';
+job = 'teacher';
+
