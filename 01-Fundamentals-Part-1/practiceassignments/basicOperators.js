9a68fa7323429089e9f34a835f935eb742cf846a
Devfarley <66633806+Devfarley@users.noreply.github.com>
2020-12-02 11:51:06 -0700

    Basic Operators

---
+++ 01-Fundamentals-Part-1/practiceassignments/basicOperators.js
@@ -0,0 +1,25 @@
+//Math Operators
+const now = 2037
+
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas);
+console.log(ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+
+const firstName = 'Jonas';
+const lastName = 'Farley';
+
+console.log(firstName + ' ' + lastName);
+
+// Assignment Operators
+let x = 10 + 5;
+x += 10;
+console.log(x);
+
+x *= 4;
+
+x++;
+// Comparison operators
+console.log(ageJonas > ageSarah);
