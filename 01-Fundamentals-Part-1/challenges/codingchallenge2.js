0d5eb5e8a7074e58017bb8c8b8617367bc3be9c2
Devfarley <66633806+Devfarley@users.noreply.github.com>
2020-12-04 12:15:37 -0700

    coding challenge 2

---
+++ 01-Fundamentals-Part-1/challenges/codingchallenge2.js
@@ -0,0 +1,8 @@
+const johnBMI = 23.9;
+const markBMI = 28.3;
+
+if (johnBMI > markBMI){
+    console.log(`John's BMI is higher than Mark's!`);
+} else {
+    console.log(`Mark's BMI is higher than John's!`)
+}
