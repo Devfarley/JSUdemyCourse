17f9c5712a1d8dfadbe81d795caa23842b727c3f
Devfarley <66633806+Devfarley@users.noreply.github.com>
2020-12-04 11:30:00 -0700

    Strings & If Else

---
+++ 01-Fundamentals-Part-1/practiceassignments/ifelse.js
@@ -0,0 +1,9 @@
+const age = 19;
+
+
+if (age >= 18){
+    console.log('Sarah can start driving license');
+} else {
+    const yearLeft = 18 - age;
+    console.log('Sarah cannot start driving license for' + yearLeft + 'years.');
+}
---
+++ 01-Fundamentals-Part-1/practiceassignments/strings.js
@@ -0,0 +1,13 @@
+const firstName = 'Jonas';
+const job = 'teacher';
+const birthYear = 1991;
+
+const jonas = "I'm " + firstName + ', a ' + (year - birthYear) + 'years old' + job + '!';
+
+console.log(jonas);
+
+const jonasNew = `I'm ${firstName}, a ${year - birthYear} year old ${job}!`;
+
+console.log(jonasNew)
+
+ 
