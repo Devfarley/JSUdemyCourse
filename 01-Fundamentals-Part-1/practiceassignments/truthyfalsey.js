09b71c138e76cfbadbeccf69b0ae9025b91b7de4
Devfarley <66633806+Devfarley@users.noreply.github.com>
2020-12-05 14:10:53 -0700

    truthy and falsey values

---
+++ 01-Fundamentals-Part-1/practiceassignments/truthyfalsey.js
@@ -0,0 +1,26 @@
+// 5 falsey values: 0, '', undefined, null, NaN
+
+console.log(Boolean(0));
+
+console.log(Boolean(undefined));
+
+console.log(Boolean('Josh'));
+
+console.log(Boolean({}));
+
+
+const money = 0;
+
+if (money) {
+    console.log("Don't spend it all.");
+} else {
+    console.log("You should get a job!");
+}
+
+let height = 0;
+
+if (height) {
+    console.log("YAY! Height is defined.");
+} else {
+    console.log("Height is UNDEFINED.");
+}
