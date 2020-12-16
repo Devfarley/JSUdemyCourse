8e2f471031e4dcadd9b84d0ec7b1c3a79a5e1841
Devfarley <66633806+Devfarley@users.noreply.github.com>
2020-12-16 11:12:47 -0700

    strict mode

--- 01-Fundamentals-Part-1/challenges/codingchallenge4.js
+++ 01-Fundamentals-Part-1/challenges/codingchallenge4.js
@@ -3,7 +3,7 @@ const bill = 430;
 let tip;
 let totalValue;
 
-bill > 50 && bill < 300 ? tip = bill * .15 : tip = bill * .2;
+bill >= 50 && bill <= 300 ? tip = bill * .15 : tip = bill * .2;
 
 
-console.log(`The bill is ${bill}, the tip is ${tip} and the total value is ${totalValue = tip + bill}`)
+console.log(`The bill is ${bill}, the tip is ${tip} and the total value is ${totalValue = tip + bill}`);
