8f3f118dcdbe5d4696872aeab88d998014ff7e92
Devfarley <66633806+Devfarley@users.noreply.github.com>
2020-12-17 13:58:57 -0700

    Arrow, declaration and expresstion functions

--- 02-Fundamentals-Part-2/practiceassignments/functions.js
+++ 02-Fundamentals-Part-2/practiceassignments/functions.js
@@ -5,4 +5,21 @@ logger = () => {
 }
 
 logger();
+// Function Declaration
+const calcAge1 = (birthYear) => {
+    return 2020 - birthYear;
+}
+
+const age1 = calcAge1(1994);
+console.log(age1);
+// Function Expression
+const calcAge2 = function (birthYear) {
+    return 2020 - birthYear;
+}
+
+const age2 = calcAge2(1994)
+
+console.log(age2)
+
+
 
