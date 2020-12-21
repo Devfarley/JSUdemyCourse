8a8ead32b403aad8f28c47be2ec30041c6985f60
Devfarley <66633806+Devfarley@users.noreply.github.com>
2020-12-21 16:22:16 -0700

    intro to arrays

---
+++ 02-Fundamentals-Part-2/practiceassignments/introtoarrays.js
@@ -0,0 +1,15 @@
+const friend1 = 'michael';
+const friend2 = 'steven';
+const friend3 = 'peter';
+
+const friends = ['michael', 'steven', 'peter'];
+console.log(friends)
+
+const years = new Array(1991, 1984, 2008, 2020);
+
+console.log(years);
+
+console.log(friends[0]);
+
+console.log(friends.length);
+
