4a9ce8bcce278d019956f8e4b9bd330f02bb50d5
Devfarley <66633806+Devfarley@users.noreply.github.com>
2020-12-23 11:42:43 -0700

    array methods

---
+++ 02-Fundamentals-Part-2/practiceassignments/arraymethods.js
@@ -0,0 +1,15 @@
+const friends = ['Cameron', 'Mike', 'Jade'];
+friends.push('Brayden');
+
+friends.unshift('John');
+
+
+friends.pop(); //last element also returns the popped element. 
+
+friends.shift(); // First element
+
+
+friends.indexOf('Mike');
+
+
+
