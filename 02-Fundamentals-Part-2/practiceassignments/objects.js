8180e32163bb5fa686cbb06676fb66e90f044749
Devfarley <66633806+Devfarley@users.noreply.github.com>
2020-12-23 12:32:28 -0700

    intro to objects

---
+++ 02-Fundamentals-Part-2/practiceassignments/objects.js
@@ -0,0 +1,20 @@
+const person = {
+    firstName : 'Josh',
+    lastName : 'Farley',
+    age : 26,
+    job : null,
+    friends : ['Mike', 'Jade', 'Brayden']
+};
+
+// Dot notation
+
+console.log(person.lastName);
+
+//Braket
+
+console.log(person['lastName']);
+
+const nameKey = 'Name';
+
+console.log(jonas['first' + nameKey]);
+
