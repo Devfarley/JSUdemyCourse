dd4fd4ab1d8ddf9d1306f924cbacda9205f74310
Devfarley <66633806+Devfarley@users.noreply.github.com>
2021-01-05 11:44:50 -0700

    Loops

--- 02-Fundamentals-Part-2/practiceassignments/loop.js
+++ 02-Fundamentals-Part-2/practiceassignments/loop.js
@@ -1,3 +1,24 @@
 for(let rep = 1; rep <= 10; rep++){
     console.log('Lifting weights repetition ' + rep)
 };
+
+
+const josh = [
+    'Josh',
+    'Farley',
+    2021-1994,
+    'Web Developer',
+    ['Jade', 'Mike', 'Brayden']
+];
+
+for (let i = josh.length - 1; i >= 0; i--){
+    console.log(josh[i])
+};
+
+for (let exercise = 1; exercise < 4;exercise++){
+    console.log('Starting exercise' + exercise)
+    
+    for (let rep = 1; rep < 6; rep++){
+        console.log('Lifting weight repetition' + rep)
+    };
+};
