e07dd3c55cf01a71d7257981e2c9012b2eea0b50
Devfarley <66633806+Devfarley@users.noreply.github.com>
2021-01-05 11:53:44 -0700

    While loops

---
+++ 02-Fundamentals-Part-2/practiceassignments/whileloops.js
@@ -0,0 +1,18 @@
+for (let rep = 1; rep <= 10; rep++){
+    console.log('Lifting weights reptition' + rep);
+};
+
+
+let rep = 1;
+while (rep <=10){
+    console.log('Lifting weights reptition' + rep);
+    rep++;
+};
+
+
+let dice = Math.trunc(Math.random() * 6) + 1;
+while (dice !== 6) {
+    console.log(dice);
+    dice = Math.trunc(Math.random() * 6) + 1;
+};
+
